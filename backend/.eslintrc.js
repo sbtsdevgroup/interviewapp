f3c14dd938f39ed68579a060abb2032343731801
ismail Muhammed <ismailmuhammed2019@gmail.com>
2026-04-03 18:17:54 +0100

    fix: update @typescript-eslint plugin reference in eslint configuration

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/plugin'],
+  plugins: ['@typescript-eslint'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
